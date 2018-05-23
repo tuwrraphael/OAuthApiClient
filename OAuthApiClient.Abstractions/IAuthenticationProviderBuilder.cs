1770fedca1601e3e5c74a145afe048ab4e045fa3
Raphael <raphael.hauk@racing.tuwien.ac.at>
2018-05-23 22:35:22 +0200

    support netstandard1.3

--- OAuthApiClient.Abstractions/IAuthenticationProviderBuilder.cs
+++ OAuthApiClient.Abstractions/IAuthenticationProviderBuilder.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿#if  NETSTANDARD2_0
+using System;
 
 namespace OAuthApiClient.Abstractions
 {
+
     public interface IAuthenticationProviderBuilder
     {
         Func<IServiceProvider, IAuthenticationProvider> GetFactory();
     }
 }
+#endif
